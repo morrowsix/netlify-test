7a333f124fb33f3f78d35187253ff90c9adcc991
thomas <derksen@moon-toon.de>
2022-10-06 09:59:46 +0200

    added tailwind

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,17 @@
-import adapter from '@sveltejs/adapter-netlify';
+import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-netlify";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter(),
+  },
+
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
 };
 
 export default config;
